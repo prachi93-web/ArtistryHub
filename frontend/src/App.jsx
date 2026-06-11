5a99be7c3cc3b68b24f638e9d9190b0d4cf71f9a
Prachi Mehetre <mehetreprachi91@gmail.com>
2026-06-11 17:26:57 +0530

    initial frontend setup and navbar implementation is done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Orders from './pages/Orders'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+         
+      </Routes>
+    </div>
+  )
+}
+
+export default App
