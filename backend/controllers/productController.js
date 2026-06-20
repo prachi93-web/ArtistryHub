a006e7a66103e7f6e0847481bb19ac0a50c94cd2
Prachi Mehetre <mehetreprachi91@gmail.com>
2026-06-20 22:50:58 +0530

    Add product controller - done

---
+++ backend/controllers/productController.js
@@ -0,0 +1,61 @@
+import { v2 as cloudinary } from 'cloudinary';
+import productModel from '../models/productModel.js'
+//add product function
+const addProduct = async (req,res) => {
+
+    try {
+        
+        const {name,description,price,category,subCategory,bestseller} = req.body;
+        console.log(req.files);
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1,image2,image3,image4].filter((item)=> item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                return result.secure_url
+            })
+        )
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === 'true' ? true : false,
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        console.log(productData);
+        
+        const product = new productModel(productData);
+        await product.save()
+
+        res.json({sucess:true,message:"Product Added sucessfully"})
+    
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+//list product function
+const listProducts = async (req,res) => {
+    
+}
+
+//remove product function
+const removeProduct = async (req,res) => {
+    
+}
+
+//display single product info function
+const singleProduct = async (req,res) => {
+    
+}
+
+export {listProducts,addProduct,singleProduct,removeProduct}
