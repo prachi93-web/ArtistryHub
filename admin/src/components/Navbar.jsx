dd60230e805482953b27c44b3dee7dcb00043734
Prachi Mehetre <mehetreprachi91@gmail.com>
2026-06-21 15:21:31 +0530

    admin panel setup, login/logout be/fe done for admin panel - done

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = ({setToken}) => {
+  return (
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+        <img className='w-24 sm:w-32 md:w-40' src={assets.logo} alt="" />
+        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>LOGOUT</button>
+    </div>
+  )
+}
+
+export default Navbar
