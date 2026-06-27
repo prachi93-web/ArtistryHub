8c5c805b48b33f404f76fe0e021457d69680019a
Prachi Mehetre <mehetreprachi91@gmail.com>
2026-06-27 14:47:10 +0530

    order placed using cod-done

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,60 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+//placing orders using COD
+const placeOrder = async (req,res) => {
+
+    try {
+        
+        const { userId ,items, amount, address } = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success: true, message: "Order Placed"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
+
+}
+
+//placing orders using Stripe Method
+const placeOrderStripe = async (req,res) => {
+    
+}
+
+//placing orders using RazorPay Method
+const placeOrderRazorpay = async (req,res) => {
+    
+}
+
+//all orders data for admin panel
+const allOrders = async (req,res) => {
+    
+}
+
+//user order data for frontend
+const userOrders = async (req,res) => {
+    
+}
+
+//update order status from Admin Panel
+const updateStatus = async (req,res) => {
+    
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
